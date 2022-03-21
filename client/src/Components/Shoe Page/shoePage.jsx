80a29d21d0aaa762128a59e840e4c60677538e01
Ilolo Izu <iloloizu97@gmail.com>
2022-03-21 13:19:49 -0500

    sneaker update

--- client/src/Components/Shoe Page/shoePage.jsx
+++ client/src/Components/Shoe Page/shoePage.jsx
@@ -2,25 +2,31 @@ import React, {useEffect,
      useState
     } from 'react';
 import { useDispatch, useSelector} from "react-redux";
+import { Card, Button } from 'react-bootstrap'
 
 function ShoePage() {
   
   const fetchFromStore = useSelector((state) => state.sneakers.entities)
 
   console.log(fetchFromStore)
-  console.log("hi")
-  // const [shoeData, setShoeData] = useState([''])
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3000/sneakers/1')
-  //     .then(r => r.json())
-  //     .then(data => setShoeData(data))
-  // }, [])  
-
-  // console.log(shoeData)
 
   return (
-    <div>shoe Page</div>
+    <div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={`${fetchFromStore[0].image}`} />
+        <Card.Body>
+          <Card.Title>{fetchFromStore[0].colorway}</Card.Title>
+          <Card.Subtitle>{fetchFromStore[0].name}</Card.Subtitle>
+          <Card.Text>
+           {fetchFromStore[0].description}
+          </Card.Text>
+           <Button variant="dark" href={fetchFromStore[0].link}>
+             
+             {"Purchase Here for $"}{fetchFromStore[0].price} </Button>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,8 @@
 
 u1 = User.create(username: "ilolo", password: "1234", address: "123 Main Street, Sugar Land, TX", email: "iloloizu97@gmail.com", size: "11", color1: "#85C1E9" , color2: "#8E44AD", phone: "8176820158")
 
-s1 = Sneaker.create(name:"Dunk Low", colorway: "Medium Curry", description: "
-    Before cementing its status as a streetwear icon and skateboarding staple, the Dunk began its journey on the hardwood as Nike's first-team basketball shoe before cementing its status as a sportswear legend. This low-top edition of the timeless silhouette features neutral hues of sail and fossil with spicy hits of medium curry.", price: 110, image:"https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/6c13023d-1771-4db5-b651-9c1798185268/dunk-low-medium-curry-release-date.jpg", link: "https://www.nike.com/launch/t/dunk-low-medium-curry" )
+s1 = Sneaker.create(name:"Dunk Low", colorway: "Medium Curry", description: "Before cementing its status as a streetwear icon and skateboarding staple,
+    the Dunk began its journey on the hardwood as Nike's first-team basketball shoe before cementing its status as a sportswear legend. This low-top 
+    edition of the timeless silhouette features neutral hues of sail and fossil with spicy hits of medium curry.", 
+    price: 110, image:"https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/6c13023d-1771-4db5-b651-9c1798185268/dunk-low-medium-curry-release-date.jpg",
+    link: "https://www.nike.com/launch/t/dunk-low-medium-curry" )
