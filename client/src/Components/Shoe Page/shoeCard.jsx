00641973e3fe672c26928719e02c3f5748e0923e
Ilolo Izu <iloloizu97@gmail.com>
2022-03-21 14:36:51 -0500

    more sneakers

--- client/src/Components/Shoe Page/shoeCard.jsx
+++ client/src/Components/Shoe Page/shoeCard.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-// import { useSelector } from "react-redux";
+import { Card, Button } from 'react-bootstrap'
 
-function ShoeCard() {
+function ShoeCard({fetchFromStore}) {
 //   const sneakerData = useSelector((state) => [...state]);
 
   // console.log(sneakerData)
 
   return (
     <>
-      <h1>Shoes</h1>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={`${fetchFromStore.image}`} />
+        <Card.Body>
+          <Card.Title>{fetchFromStore[0].colorway}</Card.Title>
+          <Card.Subtitle>{fetchFromStore[0].name}</Card.Subtitle>
+          <Card.Text>
+           {fetchFromStore[0].description}
+          </Card.Text>
+           <Button variant="dark" href={fetchFromStore[0].link}>
+             
+             {"Purchase Here for $"}{fetchFromStore[0].price} </Button>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+      </Card>
 
     </>
   );
--- client/src/Components/Shoe Page/shoePage.jsx
+++ client/src/Components/Shoe Page/shoePage.jsx
@@ -3,29 +3,29 @@ import React, {useEffect,
     } from 'react';
 import { useDispatch, useSelector} from "react-redux";
 import { Card, Button } from 'react-bootstrap'
+import ShoeCard from './shoeCard';
 
 function ShoePage() {
   
   const fetchFromStore = useSelector((state) => state.sneakers.entities)
 
-  console.log(fetchFromStore)
+  const shoeCards = fetchFromStore.map((shoe)=>
+  
+  <ShoeCard
+    // key = {shoe.id}
+    // id = {shoe.id}
+    // name={shoe.name}
+    // colorway={shoe.colorway}
+    // description={shoe.description}
+    // image={shoe.image}
+    // link={shoe.link}
+    // price={shoe.price}
+    fetchFromStore={fetchFromStore}
+  /> )
 
   return (
     <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={`${fetchFromStore[0].image}`} />
-        <Card.Body>
-          <Card.Title>{fetchFromStore[0].colorway}</Card.Title>
-          <Card.Subtitle>{fetchFromStore[0].name}</Card.Subtitle>
-          <Card.Text>
-           {fetchFromStore[0].description}
-          </Card.Text>
-           <Button variant="dark" href={fetchFromStore[0].link}>
-             
-             {"Purchase Here for $"}{fetchFromStore[0].price} </Button>
-          {/* <Button variant="primary">Go somewhere</Button> */}
-        </Card.Body>
-      </Card>
+      {shoeCards}
     </div>
   )
 }
